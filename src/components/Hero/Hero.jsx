7644975f69c9df6e1fb601c8e813f58ddb96daa8
Yana Lunina <yanalunina96@gmail.com>
2024-10-06 23:46:28 +0300

    реалізація героя

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,60 @@
+import { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import styles from './Hero.module.css';
+
 const Hero = () => {
-  return <section></section>;
+  const fullText = 'CPL Projects';
+  const [displayedText, setDisplayedText] = useState('');
+
+  useEffect(() => {
+    let index = 0;
+
+    const intervalId = setInterval(() => {
+      if (index < fullText.length) {
+        setDisplayedText(fullText.slice(0, index + 1));
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, [fullText]);
+
+  return (
+    <section className={styles.heroSection}>
+      <div className={classNames(styles['hero-wrapper'], 'container')}>
+        <h1 className={styles.heroTitle}>
+          We promote <span className={styles.heroSubText}>{displayedText}</span>
+        </h1>
+        <p className={styles.heroText}>Core competencies of the company</p>
+        <ul className={styles.heroList}>
+          <li className={styles.heroItem}>
+            <div className={styles.heroThumb}></div>
+            <p className={styles.heroItemText}>
+              Profit-focused traffic generation
+            </p>
+          </li>
+          <li className={styles.heroItem}>
+            <div className={styles.heroThumb}></div>
+            <p className={styles.heroItemText}>In-depth niche knowledge</p>
+          </li>
+          <li className={styles.heroItem}>
+            <div className={styles.heroThumb}></div>
+            <p className={styles.heroItemText}>
+              All-in-one marketing infrastructure
+            </p>
+          </li>
+          <li className={styles.heroItem}>
+            <div className={styles.heroThumb}></div>
+            <p className={styles.heroItemText}>
+              Advanced analytics with technical integrations
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
