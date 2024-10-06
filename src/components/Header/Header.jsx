6c5c8d5af16e91a95caacadfef1547dbb068af5b
Yana Lunina <yanalunina96@gmail.com>
2024-10-06 21:10:19 +0300

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,49 @@
-// import styles from "./Header.module.css";
+import { FiMenu } from "react-icons/fi";
+
+import classNames from "classnames";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>About Us</li>
-          <li>Technologies</li>
-          <li>Partnerships</li>
-          <li>Vacancies</li>
-          <li>Contacts</li>
-        </ul>
-      </nav>
-      <button type="button">Ask a Question</button>
+    <header className={styles.header}>
+      <div className={classNames(styles["header-wrapper"], "container")}>
+        <a href="./"> Logo </a>
+        <nav>
+          <ul className={styles.header_nav}>
+            <li>
+              <a href="#about" className={styles.header_link}>
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#technologies" className={styles.header_link}>
+                Technologies
+              </a>
+            </li>
+            <li>
+              <a href="#partnerships" className={styles.header_link}>
+                Partnerships
+              </a>
+            </li>
+            <li>
+              <a href="#vacancies" className={styles.header_link}>
+                Vacancies
+              </a>
+            </li>
+            <li>
+              <a href="#contacts" className={styles.header_link}>
+                Contacts
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button type="button" className={styles.header_button}>
+          Ask a Question
+        </button>
+        <button type="button" className={styles.header_burger}>
+          <FiMenu className={styles.burger_svg} />
+        </button>
+      </div>
     </header>
   );
 };
