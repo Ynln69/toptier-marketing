06771df055926bec6b73ffc9a193e9722b26ca56
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-10-07 21:40:11 +0300

    Add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,34 @@
+import classNames from 'classnames';
+import styles from './Footer.module.css';
+
 const Footer = () => {
-  <footer></footer>;
+  return (
+    <footer className={classNames('section')}>
+      <div className={classNames('container')}>
+        <h2 className={classNames(styles['contactsTitle'], 'mainTitle')}>
+          Contacts
+        </h2>
+
+        <div className={styles.contacts}>
+          <address className={styles.contactsInfo}>
+            <h3 className={styles.contactPlace}>Estonia</h3>
+            <a className={styles.contactEmail} href="mailto: info@devo.ai">
+              info@devo.ai
+            </a>
+          </address>
+
+          <div className={styles.question}>
+            <p className={styles.questionText}>
+              If you are interested in working together, contact us.
+            </p>
+            <button className={styles.questionBtn}>Ask a Question</button>
+          </div>
+        </div>
+
+        <p className={styles.copy}>Copyright &copy; 2024</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
