343e84abc69982c406c94b7b57994e3ed6446701
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-10-07 18:41:47 +0300

    Add Vacancies section

--- src/components/Vacancies/Vacancies.jsx
+++ src/components/Vacancies/Vacancies.jsx
@@ -1,5 +1,54 @@
+import classNames from 'classnames';
+import styles from './Vacancies.module.css';
+
 const Vacancies = () => {
-  return <section></section>;
+  return (
+    <section className={classNames('section')}>
+      <div className={classNames('container')}>
+        <h2 className={classNames(styles['vacanciesTitle'], 'mainTitle')}>
+          Vacancies
+        </h2>
+
+        <ul className={styles.slide}>
+          <li className={styles.vacancy}>
+            <div className={styles.content}>
+              <h3 className={styles.subTitle}>PPC Specialist</h3>
+              <p className={styles.workType}>Office + Remote</p>
+              <p className={styles.text}>
+                Key tasks: Manage and optimise PPC campaigns, conduct keyword
+                research, and analyse performance data.
+              </p>
+            </div>
+            <button className={styles.btn}>Apply for a job</button>
+          </li>
+
+          <li className={styles.vacancy}>
+            <div className={styles.content}>
+              <h3 className={styles.subTitle}>Affiliate Manager</h3>
+              <p className={styles.workType}>Office + Remote</p>
+              <p className={styles.text}>
+                Key tasks: Manage affiliate partnerships, set up new partners,
+                and analyse performance to drive growth.
+              </p>
+            </div>
+            <button className={styles.btn}>Apply for a job</button>
+          </li>
+
+          <li className={styles.vacancy}>
+            <div className={styles.content}>
+              <h3 className={styles.subTitle}>Manual QA</h3>
+              <p className={styles.workType}>Office + Remote</p>
+              <p className={styles.text}>
+                Key tasks: Perform manual testing, identify bugs, and ensure
+                product quality through detailed reporting.
+              </p>
+            </div>
+            <button className={styles.btn}>Apply for a job</button>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Vacancies;
